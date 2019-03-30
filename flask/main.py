1c1a7048bf99ba823b6889635684e70536c3465c
csaba <schoolpost2009@hotmail.com>
2019-03-30 04:03:25 -0600

    plyplot png through flask api

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,43 @@
+# from flask import Flask
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
 from flask import Flask
 app = Flask(__name__)
 
+import io
+import random
+from flask import Response
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from kernel import gen_plt
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/plot.png')
+def plot_png():
+    fig = gen_plt()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+# def create_figure():
+#     fig = Figure()
+#     axis = fig.add_subplot(1, 1, 1)
+#     xs = range(100)
+#     ys = [random.randint(1, 50) for x in xs]
+#     axis.plot(xs, ys)
+#     return fig
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
