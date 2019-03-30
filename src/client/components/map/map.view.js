b37263abf64dbd46d381693b118b7451edb692b1
Thomas Lorincz <tlorincz@ualberta.ca>
2019-03-30 12:50:27 -0600

    Layers are now selectable

--- src/client/components/map/map.view.js
+++ src/client/components/map/map.view.js
@@ -17,25 +17,63 @@ export default class MapView extends View {
       container: 'map',
       style: 'mapbox://styles/mapbox/light-v9',
       center: [-113.323975, 53.631611],
-      zoom: 4,
+      zoom: 7,
     });
 
     this.map.on('load', () => {
       this.map.addLayer({
-        'id': 'albertaLayer',
-        'type': 'fill',
-        'source': {
-          'type': 'geojson',
-          'data': 'https://s3.amazonaws.com/propel-agathos/alberta_dissemination_blocks.geojson'
+        id: 'albertaLayer',
+        source: {
+          type: 'vector',
+          url: 'mapbox://thomaslorincz.b3vm5s7s'
         },
-        'paint': {
-          'fill-color': 'rgba(200, 100, 240, 0.4)',
-          'fill-outline-color': 'rgba(200, 100, 240, 1)'
-        }
+        'source-layer': 'alberta_dissemination_blocks-b1ubfe',
+        type: 'fill',
+        paint: {
+          'fill-color': 'rgba(34,139,34,0.5)',
+          'fill-outline-color': 'rgba(34,139,34,0.2)'
+        },
+      });
+
+      this.map.addLayer({
+        id: 'albertaLayerSelected',
+        source: {
+          type: 'vector',
+          url: 'mapbox://thomaslorincz.b3vm5s7s'
+        },
+        'source-layer': 'alberta_dissemination_blocks-b1ubfe',
+        type: 'line',
+        feature_type: 'fill',
+        paint: {
+          'line-width': 6,
+          'line-color': 'black'
+        },
+        filter: ['in', 'DBUID', ''],
       });
 
       this.map.on('click', 'albertaLayer', (e) => {
-        console.log(e);
+        const features = this.map.queryRenderedFeatures(
+            [e.point.x,e.point.y],
+            'albertaLayer'
+        );
+        if (features.length > 0) {
+          let thisLayerFeatures = features.filter((d) => {
+            return d.layer.id === 'albertaLayer';
+          });
+          const feature = thisLayerFeatures[0];
+          this.map.setFilter(
+              'albertaLayerSelected',
+              ['in','DBUID', feature.properties['DBUID']]
+          );
+        }
+      });
+
+      this.map.on('mouseenter', 'albertaLayer', () => {
+        this.map.getCanvas().style.cursor = 'pointer';
+      });
+
+      this.map.on('mouseleave', 'albertaLayer', () => {
+        this.map.getCanvas().style.cursor = '';
       });
     });
   }
