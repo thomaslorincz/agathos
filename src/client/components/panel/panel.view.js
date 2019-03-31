36e67a749e6a45f06624ca9db8c143c1bc281f48
Thomas Lorincz <tlorincz@ualberta.ca>
2019-03-31 10:11:33 -0600

    Removed useless insights

--- src/client/components/panel/panel.view.js
+++ src/client/components/panel/panel.view.js
@@ -9,8 +9,6 @@ export default class PanelView extends View {
    */
   constructor(container) {
     super(container);
-    this.selected = document.getElementById('selected');
-    this.coordinates = document.getElementById('coordinates');
     this.temperature = document.getElementById('temperature');
   }
 
@@ -19,15 +17,9 @@ export default class PanelView extends View {
    */
   draw(detail) {
     if (detail) {
-      this.selected.innerText = `ID: ${detail.properties['DBUID']}`;
-      const lon = detail.geometry[0];
-      const lat = detail.geometry[1];
-      this.coordinates.innerText = `Center:\nLon: ${lon}\nLat: ${lat}`;
       this.temperature.innerText =
           `Temp: ${Math.round(detail.temp * 10) / 10}°C`;
     } else {
-      this.selected.innerText = '';
-      this.coordinates.innerText = '';
       this.temperature.innerTExt = '';
     }
   }
